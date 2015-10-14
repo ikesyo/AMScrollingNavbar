0ff3e0e24728fac978cff7a5dfb4daa161172fdc
Syo Ikeda <suicaicoca@gmail.com>
2015-10-14 12:31:33 +0900

    Backport 7b9b9bec2f41c5300e86df17a9bf1e8a6cead50a

--- Source/ScrollingNavbar+Sizes.swift
+++ Source/ScrollingNavbar+Sizes.swift
@@ -12,20 +12,7 @@ extension ScrollingNavigationController {
     }
 
     func navbarHeight() -> CGFloat {
-        let isPortrait = UIInterfaceOrientationIsPortrait(UIApplication.sharedApplication().statusBarOrientation)
-        if UIDevice().userInterfaceIdiom == .Pad || UIScreen.mainScreen().scale == 3 {
-            return portraitNavbar()
-        } else {
-            return (isPortrait ? portraitNavbar() : landscapeNavbar())
-        }
-    }
-
-    func portraitNavbar() -> CGFloat {
-        return 44 + ((self.topViewController.navigationItem.prompt != nil) ? 30 : 0)
-    }
-
-    func landscapeNavbar() -> CGFloat {
-        return 32 + ((self.topViewController.navigationItem.prompt != nil) ? 22 : 0)
+        return navigationBar.frame.size.height
     }
 
     func statusBar() -> CGFloat {
